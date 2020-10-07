dc627b8b84c55b816be9f99939444349d8a1d052
krunaltelang01@gmail.com <Admin@DESKTOP-8BLGLA0>
2020-10-07 21:57:29 +0530

    my first commit project

---
+++ src/first/tem.java
@@ -0,0 +1,18 @@
+package first;
+
+public class tem {
+
+	public static void main(String[] args) {
+
+		int i=30;
+		if(i>100)
+		{
+			System.out.println("it Hot");
+		}
+		else 
+		{
+			System.out.println("Cold");
+		}
+	}
+
+}
