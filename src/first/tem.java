f170a441b9875a67cc851667614525bed43b4f30
krunaltelang01@gmail.com <Admin@DESKTOP-8BLGLA0>
2020-10-07 22:06:18 +0530

    sd

--- src/first/tem.java
+++ src/first/tem.java
@@ -5,13 +5,17 @@ public class tem {
 	public static void main(String[] args) {
 
 		int i=30;
-		if(i>100)
+		if(i>10)
 		{
 			System.out.println("it Hot");
 		}
-		else 
+		else if(i<40)
 		{
 			System.out.println("Cold");
+		}
+		else 
+		{
+			System.out.println("hivala suru");
 		}
 	}
 
